30576a04293c132a8cc42fce3d33b44b6c2a9d96
Andres Jalife <ajalife@fi.uba.ar>
2023-04-23 12:25:38 -0300

    base and properties

---
+++ src/test/java/com/jalife/apigatewayjava/ApiGatewayJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalife.apigatewayjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
